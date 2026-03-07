274d8357e259dcfaa6bcd3eb297159372696b1ca
Diego <diegoamarilla478@gmail.com>
2026-03-07 13:13:52 -0300

    filtro por cobroDetalle

--- src/main/java/com/franco/dev/repository/operaciones/CobroDetalleRepository.java
+++ src/main/java/com/franco/dev/repository/operaciones/CobroDetalleRepository.java
@@ -26,10 +26,15 @@ public interface CobroDetalleRepository extends HelperRepository<CobroDetalle, E
         // Query sin filtros (datos históricos completos)
         @Query(value = "SELECT fp.id as forma_pago_id, " +
                         "fp.descripcion, " +
-                        "COUNT(cd.id) as cantidad_transacciones, " +
+                        "COUNT(DISTINCT cd.venta_id) as cantidad_transacciones, " +
                         "COALESCE(SUM(cd.valor), 0) as total_monto " +
                         "FROM financiero.forma_pago fp " +
-                        "LEFT JOIN operaciones.cobro_detalle cd ON cd.forma_pago_id = fp.id AND cd.pago = true " +
+                        "LEFT JOIN ( " +
+                        "    SELECT cd2.forma_pago_id, cd2.valor, v2.id as venta_id, cd2.sucursal_id " +
+                        "    FROM operaciones.cobro_detalle cd2 " +
+                        "    JOIN operaciones.venta v2 ON v2.cobro_id = cd2.cobro_id AND v2.sucursal_id = cd2.sucursal_id " +
+                        "    WHERE v2.estado = 'CONCLUIDA' AND (cd2.pago = true OR cd2.vuelto = true) " +
+                        ") cd ON cd.forma_pago_id = fp.id " +
                         "WHERE fp.activo = true " +
                         "GROUP BY fp.id, fp.descripcion " +
                         "ORDER BY total_monto DESC", nativeQuery = true)
@@ -38,11 +43,16 @@ public interface CobroDetalleRepository extends HelperRepository<CobroDetalle, E
         // Query por sucursal sin filtros de fecha
         @Query(value = "SELECT fp.id as forma_pago_id, " +
                         "fp.descripcion, " +
-                        "COUNT(cd.id) as cantidad_transacciones, " +
+                        "COUNT(DISTINCT cd.venta_id) as cantidad_transacciones, " +
                         "COALESCE(SUM(cd.valor), 0) as total_monto " +
                         "FROM financiero.forma_pago fp " +
-                        "LEFT JOIN operaciones.cobro_detalle cd ON cd.forma_pago_id = fp.id AND cd.pago = true " +
-                        "WHERE fp.activo = true AND cd.sucursal_id = ?1 " +
+                        "LEFT JOIN ( " +
+                        "    SELECT cd2.forma_pago_id, cd2.valor, v2.id as venta_id, cd2.sucursal_id " +
+                        "    FROM operaciones.cobro_detalle cd2 " +
+                        "    JOIN operaciones.venta v2 ON v2.cobro_id = cd2.cobro_id AND v2.sucursal_id = cd2.sucursal_id " +
+                        "    WHERE v2.estado = 'CONCLUIDA' AND (cd2.pago = true OR cd2.vuelto = true) " +
+                        ") cd ON cd.forma_pago_id = fp.id AND cd.sucursal_id = ?1 " +
+                        "WHERE fp.activo = true " +
                         "GROUP BY fp.id, fp.descripcion " +
                         "ORDER BY total_monto DESC", nativeQuery = true)
         List<Object[]> obtenerEstadisticasFormaPagoPorSucursal(Long sucursalId);
@@ -50,10 +60,15 @@ public interface CobroDetalleRepository extends HelperRepository<CobroDetalle, E
         // Query con filtros de fecha (todas las sucursales)
         @Query(value = "SELECT fp.id as forma_pago_id, " +
                         "fp.descripcion, " +
-                        "COUNT(cd.id) as cantidad_transacciones, " +
+                        "COUNT(DISTINCT cd.venta_id) as cantidad_transacciones, " +
                         "COALESCE(SUM(cd.valor), 0) as total_monto " +
                         "FROM financiero.forma_pago fp " +
-                        "LEFT JOIN operaciones.cobro_detalle cd ON cd.forma_pago_id = fp.id AND cd.pago = true " +
+                        "LEFT JOIN ( " +
+                        "    SELECT cd2.forma_pago_id, cd2.valor, v2.id as venta_id, cd2.creado_en " +
+                        "    FROM operaciones.cobro_detalle cd2 " +
+                        "    JOIN operaciones.venta v2 ON v2.cobro_id = cd2.cobro_id AND v2.sucursal_id = cd2.sucursal_id " +
+                        "    WHERE v2.estado = 'CONCLUIDA' AND (cd2.pago = true OR cd2.vuelto = true) " +
+                        ") cd ON cd.forma_pago_id = fp.id " +
                         "    AND cd.creado_en >= ?1 AND cd.creado_en < ?2 " +
                         "WHERE fp.activo = true " +
                         "GROUP BY fp.id, fp.descripcion " +
@@ -63,10 +78,15 @@ public interface CobroDetalleRepository extends HelperRepository<CobroDetalle, E
         // Query con filtros de fecha y sucursal
         @Query(value = "SELECT fp.id as forma_pago_id, " +
                         "fp.descripcion, " +
-                        "COUNT(cd.id) as cantidad_transacciones, " +
+                        "COUNT(DISTINCT cd.venta_id) as cantidad_transacciones, " +
                         "COALESCE(SUM(cd.valor), 0) as total_monto " +
                         "FROM financiero.forma_pago fp " +
-                        "LEFT JOIN operaciones.cobro_detalle cd ON cd.forma_pago_id = fp.id AND cd.pago = true " +
+                        "LEFT JOIN ( " +
+                        "    SELECT cd2.forma_pago_id, cd2.valor, v2.id as venta_id, cd2.sucursal_id, cd2.creado_en " +
+                        "    FROM operaciones.cobro_detalle cd2 " +
+                        "    JOIN operaciones.venta v2 ON v2.cobro_id = cd2.cobro_id AND v2.sucursal_id = cd2.sucursal_id " +
+                        "    WHERE v2.estado = 'CONCLUIDA' AND (cd2.pago = true OR cd2.vuelto = true) " +
+                        ") cd ON cd.forma_pago_id = fp.id " +
                         "    AND cd.creado_en >= ?1 AND cd.creado_en < ?2 " +
                         "    AND cd.sucursal_id = ?3 " +
                         "WHERE fp.activo = true " +

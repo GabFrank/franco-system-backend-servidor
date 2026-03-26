aca37cffdaf3a2853444e1423bb33a2dadf22a09
Maurolando <maurorrf06@gmail.com>
2026-03-26 15:23:40 -0300

    fd-126: add creadoEn en ProveedorInput

--- src/main/java/com/franco/dev/graphql/personas/input/ProveedorInput.java
+++ src/main/java/com/franco/dev/graphql/personas/input/ProveedorInput.java
@@ -1,5 +1,7 @@
 package com.franco.dev.graphql.personas.input;
 
+import java.time.LocalDateTime;
+
 import lombok.Data;
 
 @Data
@@ -11,4 +13,5 @@ public class ProveedorInput {
     private Long datosBancariosId;
     private Long personaId;
     private Long usuarioId;
+    private LocalDateTime creadoEn;
 }

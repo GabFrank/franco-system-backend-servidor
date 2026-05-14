5669210990ece4a4eaff85bc014b2c95d3c39c39
Diego Amarilla <diegoamarilla478@gmail.com>
2026-05-14 13:34:05 -0300

    fix(notificaciones): configuracion-firebase

--- src/main/java/com/franco/dev/graphql/personas/resolver/UsuarioResolver.java
+++ src/main/java/com/franco/dev/graphql/personas/resolver/UsuarioResolver.java
@@ -1,10 +1,8 @@
 package com.franco.dev.graphql.personas.resolver;
 
 import com.franco.dev.domain.configuracion.InicioSesion;
-import com.franco.dev.domain.personas.Persona;
 import com.franco.dev.domain.personas.Role;
 import com.franco.dev.domain.personas.Usuario;
-import com.franco.dev.domain.personas.UsuarioRole;
 import com.franco.dev.service.configuracion.InicioSesionService;
 import com.franco.dev.service.personas.PersonaService;
 import com.franco.dev.service.personas.RoleService;
@@ -17,10 +15,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class UsuarioResolver implements GraphQLResolver<Usuario> {
@@ -43,19 +39,19 @@ public class UsuarioResolver implements GraphQLResolver<Usuario> {
     @Autowired
     private Environment env;
 
-    public List<String> roles(Usuario u){
+    public List<String> roles(Usuario u) {
         List<Role> roleList = roleService.findByUsuarioId(u.getId());
         List<String> roleStringList = new ArrayList<>();
-        for(Role r: roleList){
+        for (Role r : roleList) {
             roleStringList.add(r.getNombre());
         }
         return roleStringList;
     }
 
-    public InicioSesion inicioSesion(Usuario u){
-        Page<InicioSesion> inicioSesionPage =
-                inicioSesionService.findByUsuarioIdAndHoraFinIsNul(u.getId(), Long.valueOf(env.getProperty("sucursalId")), PageRequest.of(0, 10));
-        if(inicioSesionPage.getNumberOfElements() > 0){
+    public InicioSesion inicioSesion(Usuario u) {
+        Page<InicioSesion> inicioSesionPage = inicioSesionService.findByUsuarioIdAndHoraFinIsNul(u.getId(), null,
+                PageRequest.of(0, 10));
+        if (inicioSesionPage.getNumberOfElements() > 0) {
             return inicioSesionPage.getContent().get(0);
         } else {
             return null;
